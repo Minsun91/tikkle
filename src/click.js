ac5063877aba72725128c13e7dc499feb990f35d
Minsun91 <minsunkim167@gmail.com>
2024-02-22 17:44:05 +0100

    qKr

--- src/click.js
+++ src/click.js
@@ -53,77 +53,77 @@ import './styles/click.css';
 import './styles/button.scss';
 
 function openUrlInIncognito(url) {
-  window.open(url, "_blank", "toolbar=0,location=0,menubar=0");
+    window.open(url, "_blank", "toolbar=0,location=0,menubar=0");
 }
 function onClick() {
     confetti({
-      particleCount: 150,
-      spread: 60
+        particleCount: 150,
+        spread: 60
     });
-  }
+}
 
 export default function Click() {
-  const currentLinkIndexRef = useRef(-1);
-  const [linkOpenCount, setLinkOpenCount] = useState(0);
-  const [coins, setCoins] = useState([]);
+    const currentLinkIndexRef = useRef(-1);
+    const [linkOpenCount, setLinkOpenCount] = useState(0);
+    const [coins, setCoins] = useState([]);
 
-  useEffect(() => {
-      const openNextLink = () => {
-          let randomIndex;
-          do {
-              randomIndex = Math.floor(Math.random() * links.length);
-          } while (currentLinkIndexRef.current === randomIndex);
-          
-          const currentTime = new Date().toLocaleTimeString();
-          setLinkOpenCount(prevCount => prevCount + 1);
-          setCoins(prevCoins => [...prevCoins, {
-              id: linkOpenCount,
-              time: currentTime
-          }]);
-          console.log(`[${currentTime}] Opening ${links[randomIndex]}. Link opened ${linkOpenCount} times.`);
-          openUrlInIncognito(links[randomIndex]);
-          currentLinkIndexRef.current = randomIndex;
-      };
-      
-      const interval = setInterval(openNextLink, 200000); // 200초마다 링크 열기
-      return () => clearInterval(interval);
-  }, [linkOpenCount]); // linkOpenCount가 변경될 때마다 useEffect가 다시 실행되도록 함
+    useEffect(() => {
+        const openNextLink = () => {
+            let randomIndex;
+            do {
+                randomIndex = Math.floor(Math.random() * links.length);
+            } while (currentLinkIndexRef.current === randomIndex);
 
-  useEffect(() => {
-      console.log("Coins updated:", coins); // coins 상태값이 업데이트될 때마다 콘솔에 메시지 출력
-  }, [coins]); // coins 상태값이 변경될 때마다 useEffect가 다시 실행되도록 함
+            const currentTime = new Date().toLocaleTimeString();
+            setLinkOpenCount(prevCount => prevCount + 1);
+            setCoins(prevCoins => [...prevCoins, {
+                id: linkOpenCount,
+                time: currentTime
+            }]);
+            console.log(`[${currentTime}] Opening ${links[randomIndex]}. Link opened ${linkOpenCount} times.`);
+            openUrlInIncognito(links[randomIndex]);
+            currentLinkIndexRef.current = randomIndex;
+        };
 
-  const handleOpenPopup = () => {
-      const randomIndex = Math.floor(Math.random() * links.length);
-      const link = links[randomIndex];
-      openUrlInIncognito(link); // 팝업 열기
-      setCoins(prevCoins => [{
-          id: linkOpenCount,
-          time: new Date().toLocaleTimeString()
-      }, ...prevCoins]); // 동전 쌓기
-      setLinkOpenCount(prevCount => prevCount + 1);
-  };
+        const interval = setInterval(openNextLink, 200000); // 200초마다 링크 열기
+        return () => clearInterval(interval);
+    }, [linkOpenCount]); // linkOpenCount가 변경될 때마다 useEffect가 다시 실행되도록 함
 
-  return (
-      <div>
-          <h1>Let's turn dust into Money!🤑</h1>
-          <p>Open this page in a new Incognito window. </p>
-          <div className="CoinContainer">
-              <TransitionGroup> {/* coins를 TransitionGroup으로 감싸서 애니메이션 효과 추가 */}
-                  {coins.map(coin => (
-                      <CSSTransition
-                          key={coin.id}
-                          classNames="coin-fall"
-                          timeout={450}
-                      >
-                          <div className="Coin" key={coin.id}></div>
-                      </CSSTransition>
-                  ))}
-              </TransitionGroup>
-          </div>
-<button onClick={() => { handleOpenPopup(); onClick(); }} data-text="Add Tikkle"> Add Tikkle </button>
+    useEffect(() => {
+        console.log("Coins updated:", coins); // coins 상태값이 업데이트될 때마다 콘솔에 메시지 출력
+    }, [coins]); // coins 상태값이 변경될 때마다 useEffect가 다시 실행되도록 함
 
-          {/* <button onClick={handleOpenPopup} data-text="Add Tikkle"> Add Tikkle </button> */}
-      </div>
-  );
+    const handleOpenPopup = () => {
+        const randomIndex = Math.floor(Math.random() * links.length);
+        const link = links[randomIndex];
+        openUrlInIncognito(link); // 팝업 열기
+        setCoins(prevCoins => [{
+            id: linkOpenCount,
+            time: new Date().toLocaleTimeString()
+        }, ...prevCoins]); // 동전 쌓기
+        setLinkOpenCount(prevCount => prevCount + 1);
+    };
+
+    return (
+        <div>
+            <h1>Let's turn dust into Money!🤑</h1>
+            <p>Open this page in a new Incognito window. </p>
+            <div className="CoinContainer">
+                <TransitionGroup> {/* coins를 TransitionGroup으로 감싸서 애니메이션 효과 추가 */}
+                    {coins.map(coin => (
+                        <CSSTransition
+                            key={coin.id}
+                            classNames="coin-fall"
+                            timeout={450}
+                        >
+                            <div className="Coin" key={coin.id}></div>
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
+            </div>
+            <button onClick={() => { handleOpenPopup(); onClick(); }} data-text="Add Tikkle"> Add Tikkle </button>
+
+            {/* <button onClick={handleOpenPopup} data-text="Add Tikkle"> Add Tikkle </button> */}
+        </div>
+    );
 }
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',    // 배포할때는 'production'변경
+    devtool: 'eval',        // eval은 빌드하는 속도를 빠르게 해준다. 배포할때는 'hidden-source-map' 변경
+    watch: true,     // 저장할때마다 자동으로 빌드 해준다.!
+    context: __dirname, 
+    resolve: {       
+        extensions: ['.js', '.css', '.scss', '.html']
+    },
+    entry: {
+        app: path.join(__dirname, 'src', 'App.js'),
+    },    
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }        
+        ]
+      }
+      ,
+      plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    
+    output: {
+        publicPath: '/dist/',    //웹 서버에서 배포할 기본 경로를 뜻한다.  webpack-dev-server을 사용할때 추가해줘야 한다. 안그러면 에러난다.
+        filename: '[name].js',   //[name].js으로 해주면 아라서 위에 entry에 있는 app이 [name].js로 들어간다.
+        path: path.join(__dirname, 'dist')
+    },
+};
